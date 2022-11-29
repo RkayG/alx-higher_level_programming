5a90e3314f911351da8f3c85d19cd8005284605c
Your Name <you@example.com>
2022-11-29 13:20:19 -0800

    more corrections

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,4 +2,5 @@
 for i in range(0, 100):
     if i == 99:
         print("{}".format(i))
-    print(f"{i:02d}", end=", ")
+    else:
+        print(f"{i:02d}", end=", ")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(len(str)):
-        if str[i] >= chr(97) and str[i] <= chr(122):
-            str = chr(ord(str[i]) - 32)
-    return str[i]
+    for i in (str):
+        if ord(i) >= 97 and ord(i) <= 122:
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print("")
