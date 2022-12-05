5ae686a43094e687b9fb33a5b17f131dcd5d2197
Your Name <you@example.com>
2022-12-05 10:43:29 -0800

    task 8

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     """finds all multiples of 2 in a list"""
-    if my_list:
-        values = []
-        for i in my_list:
-            if i % 2 is 0:
-                values.append(True)
-            else:
-                values.append(False)
-        return values
+    return [(i % 2 == 0) for i in my_list]
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        sentence[0] = None
-    tuple_sen = (len(sentence), sentence[0])
-    return (tuple_sen)
+    """ returns a tuple with the length of a string and its
+        first character.
+    """
+    length = len(sentence)
+    char = sentence[0] if length > 0 else None
+
+    return (length, char)
