98010ab69640b24a1f94b0219ecf26ce30cfdfac
Your Name <you@example.com>
2022-12-06 08:23:35 -0800

    used static array in linked list

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,27 +10,21 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *current = *head;
 	int arrayNum[10240];
-	int i = 0, j = 0, m, size = 0;
+	int i = 0, j = 0, m;
 
 	if (head == NULL) /* list doesn't exist */
 		return (0);
 	if (*head == NULL)/* if empty list, then it's a palindrome */
 		return (1);
-
-	while (current != NULL)
-	{
-		current = current->next;
-		size++;
-	}
-	if (size == 1)
-		return (1);
-
+	/* store nodes in array */
 	while (current != NULL)
 	{
 		arrayNum[i] = current->n;
 		current = current->next;
 		i++;
 	}
+	if (i == 1)/*single list is a palimdrome*/
+		return (1);
 	i--;
 	m = i / 2;
 	for (; j <= m; j++)
