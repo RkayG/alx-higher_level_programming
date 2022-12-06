4c28d3fef2ac5e8d65754692931aee59370cfcc4
Your Name <you@example.com>
2022-12-06 07:35:51 -0800

    linked list

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: pointer to head node
+ * Return: 1 if palindrome, otherwise 0
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *current = *head;
+	int arrayNum[100];
+	int i = 0, j = 0, m;
+
+	while (current != NULL)
+	{
+		arrayNum[i] = current->n;
+		current = current->next;
+		i++;
+	}
+	if (i % 2 != 0)
+		return (0);
+	i--;
+	m = i / 2;
+	for (; j <= m; j++)
+	{
+		if (arrayNum[j] != arrayNum[i])
+			return (0);
+		i--;
+	}
+	return (1);
+}
+
